bdaff7ef626d1bba32d09ab48796a1af756d2586
Roxana-L <roxana.lupei311@gmail.com>
2021-05-16 17:46:35 +0100

    Created forecast for the first day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,20 +1,34 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
-import "./WeatherForecast.css";
+import React, { useState } from "react";
 
-export default function WeatherForecast() {
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div classname="ForecastDay">Mon</div>
-          <WeatherIcon code="01d" size={36} />
-          <div className="ForecastTemperature">
-            <span className="ForecastTemperature-max">15°</span>
-            <span className="ForecastTemperature-min">10°</span>
+import WeatherForecastDay from "./WeatherForecastDay";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "65a91898f0adac0e8022281b9adad7cc";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
